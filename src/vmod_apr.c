b16f7644ce41b735472049c873914a3ccc5012dc
Jesse Sipprell <jsipprell@mcclatchyinteractive.com>
2014-03-25 19:11:54 -0400

    testing basic apr linkage

--- src/vmod_apr.c
+++ src/vmod_apr.c
@@ -8,37 +8,134 @@
 #endif
 
 typedef struct {
-  void *pool;
+  apr_pool_t *pool,*global_pool;
+  apr_thread_mutex_t *mutex;
+  volatile apr_uint32_t *refcount;
 } apr_config_t;
+
+static apr_status_t indirect_wipe(void *pptr)
+{
+  if(pptr)
+    *((void**)pptr) = NULL;
+  return APR_SUCCESS;
+}
+
+static apr_status_t cleanup_config(void *cfgptr)
+{
+  apr_config_t *cfg = (apr_config_t*)cfgptr;
+
+  cfg->pool = cfg->global_pool = NULL;
+  cfg->refcount = NULL;
+  cfg->mutex = NULL;
+  return APR_SUCCESS;
+}
 
 static apr_config_t *make_config(void)
 {
+  static volatile apr_uint32_t refcount = 0;
+  static apr_pool_t *global_pool = NULL;
+  static apr_thread_mutex_t *global_mutex = NULL;
   apr_config_t *cfg;
   LOG_T("make config\n");
 
-  cfg = malloc(sizeof(apr_config_t));
-  if(cfg != NULL) {
-    cfg->pool = malloc(1024);
-    assert(cfg->pool != NULL);
+  if(apr_atomic_read32(&refcount) == 0 && global_pool == NULL) {
+    static volatile apr_uint32_t spinlock = 0;
+
+    while(apr_atomic_cas32(&spinlock,1,0) != 0)
+      ;
+    if(global_pool == NULL) {
+      /* apr_atomic_set32(&refcount,1); */
+      ASSERT_APR(apr_initialize());
+      ASSERT_APR(apr_pool_create(&global_pool,NULL));
+      AN(global_pool);
+
+      ASSERT_APR(apr_thread_mutex_create(&global_mutex,APR_THREAD_MUTEX_NESTED,global_pool));
+      AN(global_mutex);
+
+      apr_pool_cleanup_register(global_pool,&global_mutex,
+                                indirect_wipe,apr_pool_cleanup_null);
+      apr_pool_cleanup_register(global_pool,&global_pool,
+                                indirect_wipe,apr_pool_cleanup_null);
+    }
+    AZ(apr_atomic_dec32(&spinlock));
   }
+
+  ASSERT_APR(apr_thread_mutex_lock(global_mutex));
+  AN(cfg = apr_palloc(global_pool,sizeof(apr_config_t)));
+  cfg->global_pool = global_pool;
+  cfg->pool = NULL;
+  cfg->mutex = global_mutex;
+  cfg->refcount = &refcount;
+  ASSERT_APR(apr_pool_create(&cfg->pool,global_pool));
+  apr_pool_cleanup_register(cfg->pool,cfg,cleanup_config,apr_pool_cleanup_null);
+  ASSERT_APR(apr_thread_mutex_unlock(global_mutex));
+  apr_atomic_inc32(cfg->refcount);
+  LOG_T("config refcount: %u\n",(unsigned)apr_atomic_read32(&refcount));
+
   return cfg;
 }
 
 static void free_config(apr_config_t *cfg)
 {
-  if(cfg) {
-    if(cfg->pool) {
-      free(cfg->pool);
-      cfg->pool = NULL;
+  static volatile apr_uint32_t *refcount = NULL;
+  static apr_pool_t *global_pool = NULL;
+
+#ifdef DEBUG
+  if(refcount) {
+    LOG_T("free_config, start refcount %u\n",(unsigned)apr_atomic_read32(refcount));
+  } else {
+    LOG_T("free_config, unknown refcount (yet)\n");
+  }
+#endif
+
+  if (refcount == NULL && cfg && cfg->refcount) {
+    static volatile apr_uint32_t spinlock = 0;
+    while(apr_atomic_cas32(&spinlock,0,1) != 0)
+      ;
+    if(refcount == NULL)
+      refcount = cfg->refcount;
+    assert(apr_atomic_cas32(&spinlock,1,0) == 1);
+    ASSERT_APR(apr_thread_mutex_unlock(cfg->mutex));
+    LOG_T("free_config, got refcount %u\n",(unsigned)apr_atomic_read32(refcount));
+  }
+  if (cfg) {
+    apr_pool_t *p = cfg->pool;
+    apr_thread_mutex_t *mutex = cfg->mutex;
+
+    if(cfg->global_pool != global_pool)
+      global_pool = cfg->global_pool;
+
+    if(p) {
+      ASSERT_APR(apr_thread_mutex_lock(mutex));
+      apr_pool_destroy(p);
+      ASSERT_APR(apr_thread_mutex_unlock(mutex));
     }
-    free(cfg);
+  }
+  if(!refcount || apr_atomic_dec32(refcount) == 0) {
+    apr_pool_t *p = global_pool;
+    LOG_T("free_config: refcount %u, terminating apr.\n",(unsigned)apr_atomic_read32(refcount));
+    global_pool = NULL;
+    if(p != NULL)
+      apr_pool_destroy(p);
+    apr_terminate();
+  } else {
+    LOG_T("free_config finished, refcount %u\n",(unsigned)apr_atomic_read32(refcount));
   }
 }
 
 int init_function(struct vmod_priv *priv, const struct VCL_conf *conf)
 {
+  apr_config_t *p = priv->priv;
+
   LOG_T("apr init\n");
+  if(p) {
+    LOG_T("double init? priv already exists!\n");
+    priv->priv = NULL;
+    free_config(p);
+  }
+  priv->priv = make_config();
   priv->free = (vmod_priv_free_f*)free_config;
+
   return 0;
 }
 
--- src/vmod_apr.h
+++ src/vmod_apr.h
@@ -19,6 +19,10 @@
 #include <apr_time.h>
 #include <apr_general.h>
 #include <apr_lib.h>
+#include <apr_errno.h>
+#include <apr_atomic.h>
+#include <apr_thread_proc.h>
+#include <apr_thread_mutex.h>
 #include <apr_want.h>
 
 #ifdef HAVE_STDLIB_H
@@ -42,4 +46,14 @@
 
 #include "vcc_if.h"
 
-#endif
+#ifdef WITHOUT_ASSERTS
+#define ASSERT_APR(op) ((void)(op))
+#else
+#define ASSERT_APR(op) do { \
+  apr_status_t st = (op); \
+  if(st != APR_SUCCESS) \
+    VAS_Fail(__func__, __FILE__, __LINE__, #op, APR_TO_OS_ERROR(st), 0); \
+} while(0)
+#endif /* WITHOUT_ASSERTS */
+
+#endif /* LIBVMOD_APR_H */
